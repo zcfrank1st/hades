963d2c79534177d02938f6966a00208fd72e0765
zcfrank1st <zcfrank1st@sina.com>
2016-11-23 11:49:24 +0800

    add cors

---
+++ hades-web/src/main/java/com/chaos/hades/web/configuration/CORSConfiguration.java
@@ -0,0 +1,26 @@
+package com.chaos.hades.web.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+/**
+ * Created by zcfrank1st on 23/11/2016.
+ */
+@Configuration
+public class CORSConfiguration {
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedHeaders("*")
+                        .allowedMethods("*")
+                        .allowedOrigins("*");
+            }
+        };
+    }
+}
